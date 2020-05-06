f388016c6ed569db255453f8a37cda38a0607133
Maitrey1302 <64709380+Maitrey1302@users.noreply.github.com>
2020-05-06 05:56:11 +0530

    For loop
    repetition of number

---
+++ For loop.c
@@ -0,0 +1,19 @@
+//ame-Maitrey Pangal
+//email id- marshpangal@gmail.com
+include <stdio.h>
+
+int main()
+{
+	int i,j;
+	i=0;j=0;
+	for(i=1;i<=7;i++)
+	{
+		for(j=1;j<i;j++)
+		{
+			printf("01");
+		}
+		printf("\n");
+	}
+
+    return 0;
+}
